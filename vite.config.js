99897c81ae59943a7b37431e952b466c6fd8e4d1
Shanta Ahmed <bhuiyanshanta@gmail.com>
2026-04-18 22:17:02 +0600

    removed base path for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Assignment-7/",
-  build: {
-    outDir: "dist"
-  }
 })
